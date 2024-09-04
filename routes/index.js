69126df47739b7b9fa08f4ffd163546378f0044c
Nanakzy <nanakzy@gmail.com>
2024-09-04 09:56:28 -0700

    task3

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-//import UsersController from '../controllers/UsersController';
+import UsersController from '../controllers/UsersController';
 //import AuthController from '../controllers/AuthController';
 
 const router = Router();
@@ -10,6 +10,6 @@ router.get('/stats', AppController.getStats);
 //router.get('/connect', AuthController.getConnect);
 //router.get('/disconnect', AuthController.getDisconnect);
 //router.get('/users/me', UsersController.getMe);
-//router.post('/users', UsersController.postNew);
+router.post('/users', UsersController.postNew);
 
 export default router;
