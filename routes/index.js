6cb949c415fb4eff9c9ba82dc77c844724d19d70
Nanakzy <nanakzy@gmail.com>
2024-09-04 10:08:04 -0700

    bulk commit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,39 @@
-import { Router } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
-//import AuthController from '../controllers/AuthController';
 
-const router = Router();
+const router = express.Router();
 
+// GET /status => AppController.getStatus
 router.get('/status', AppController.getStatus);
+// GET /stats => AppController.getStats
 router.get('/stats', AppController.getStats);
-//router.get('/connect', AuthController.getConnect);
-//router.get('/disconnect', AuthController.getDisconnect);
-//router.get('/users/me', UsersController.getMe);
-router.post('/users', UsersController.postNew);
 
-export default router;
+// POST /users => UsersController.postNew
+router.post('/users', UsersController.postNew);
+// GET /users/me => UserController.getMe
+router.get('/users/me', UsersController.getMe);
+
+// GET /connect => AuthController.getConnect
+router.get('/connect', AuthController.getConnect);
+// GET /disconnect => AuthController.getDisconnect
+router.get('/disconnect', AuthController.getDisconnect);
+
+// POST /files => FilesController.postUpload
+router.post('/files', FilesController.postUpload);
+// GET /files/:id => FilesController.getShow
+router.get('/files/:id', FilesController.getShow);
+// GET /files => FilesController.getIndex
+router.get('/files', FilesController.getIndex);
+// PUT /files/:id/publish => FilesController.putPublish
+router.put('/files/:id/publish', FilesController.putPublish);
+// PUT /files/:id/publish => FilesController.putUnpublish
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+// GET /files/:id/data => FilesController.getFile
+router.get('/files/:id/data', FilesController.getFile);
+// GET /files/:id/data
+router.get('/files/:id/data', FilesController.getFile);
+
+module.exports = router;
